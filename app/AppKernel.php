7b3c714a63266c044595429924846eacfc7de6d4
Thibaut Cuvelier <dourouc05@gmail.com>
2011-06-08 21:29:59 +0200

    f

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new AntiMattr\GoogleBundle\GoogleBundle(),
+            // new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test')))
--- app/autoload.php
+++ app/autoload.php
@@ -4,17 +4,18 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/metadata/src',
-    'AntiMattr'        => __DIR__.'/../vendor/bundles', 
-    'Sf2gen'           => __DIR__.'/../vendor/bundles',
+    'Symfony'           => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Sensio'            => __DIR__.'/../vendor/bundles',
+    'JMS'               => __DIR__.'/../vendor/bundles',
+    'Doctrine\\Common'  => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL'    => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'          => __DIR__.'/../vendor/doctrine/lib',
+    'Monolog'           => __DIR__.'/../vendor/monolog/src',
+    'Assetic'           => __DIR__.'/../vendor/assetic/src',
+    'Metadata'          => __DIR__.'/../vendor/metadata/src',
+    'AntiMattr'         => __DIR__.'/../vendor/bundles', 
+    'Sf2gen'            => __DIR__.'/../vendor/bundles',
+    'FOS'               => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
