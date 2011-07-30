7e71ecca8200095b9dde59c27fa761878000adc3
Thibaut Cuvelier <dourouc05@gmail.com>
2011-07-30 22:01:06 +0200

    fs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             // new FOS\UserBundle\FOSUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Quiz\QuizBundle\QuizQuizBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
