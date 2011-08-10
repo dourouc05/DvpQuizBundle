eb0fe8739655ac04d2a4a36149ab7f4eca7b2a93
Thibaut Cuvelier <dourouc05@gmail.com>
2011-08-10 20:08:06 +0200

    42

--- src/Quiz/QuizBundle/Security/LoginFormPreAuthenticateListener.php
+++ src/Quiz/QuizBundle/Security/LoginFormPreAuthenticateListener.php
@@ -16,23 +16,37 @@ class LoginFormPreAuthenticateListener
         $rq = $event->getRequest()->request;
         if($rq->has('_password') && $rq->has('_username'))
         {
-            $xml = new \SimpleXMLElement
-                (
-                    file_get_contents
-                    (
-                        'http://www.developpez.net/forums/anologin.php?pseudo=' . $rq->get('_username') . '&motdepasse=' . $rq->get('_username')
-                    )
-                );
+//            $xml = 'http://www.developpez.net/forums/anologin.php?pseudo=' . $rq->get('_username') . '&motdepasse=' . $rq->get('_password');
+//            var_dump($xml);
+//            $xml = file_get_contents($xml);
+//            var_dump($xml);
+//            $xml = new \SimpleXMLElement($xml);
+//            var_dump($xml);
             
             if(0 == $xml->erreur)
+            {
                 throw new BadCredentialsException($xml->erreur); 
+            }
+            else
+            {
+                $entityManager = $this->get('doctrine.orm.entity_manager');
+                $user = new User();
+                $user->setAlgorithm('is_scalar');
+                $user->setId($xml->id);
+                $user->setUsername($xml->pseudo);
+                $user->setEmail($xml->email);
+                $user->setFirstName($xml->prenom);
+                $user->setName($xml->nom);
+                //$xml->redac
+                //$xml->resp
+                //$xml->admin
+            }
             // $rq->set($id, $val)
-            var_dump($xml);
+//            var_dump($xml);
 
 //            $userManager = $container->get('fos_user.user_manager');
 //            $user = $userManager->createUser();
-            $user = new User();
-            $user->setAlgorithm('is_scalar');
+            
         }
 
             exit;
