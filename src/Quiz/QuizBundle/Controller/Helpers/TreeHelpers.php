ec61b743b2612c3cdb59c5494c171e9e680d76b2
Thibaut Cuvelier <dourouc05@gmail.com>
2011-08-16 13:32:29 +0200

    deleted html output for tree helpers

--- src/Quiz/QuizBundle/Controller/Helpers/TreeHelpers.php
+++ src/Quiz/QuizBundle/Controller/Helpers/TreeHelpers.php
@@ -14,23 +14,6 @@ class TreeHelpers
         $this->cache->setNamespace('helpers.tree..');
     }
     
-    public function treeShow()
-    {
-        if($this->cache->contains('full.html'))
-        {
-            return $this->cache->fetch('full.html');
-        }
-        else
-        {
-            $repo  = $this->em->getRepository('\Quiz\QuizBundle\Entity\Category');
-            $roots = $repo->getRootNodes();
-            $cache = $this->subTreeShow($roots, $repo);
-            $this->cache->save('full.html', $cache); // Le cache n'expire jamais : 
-            // on élimine ce qu'il faut à chaque création de catégorie. 
-            return $cache;
-        }
-    }
-    
     public function treeContents()
     {
         if($this->cache->contains('full.array'))
@@ -45,24 +28,6 @@ class TreeHelpers
             $this->cache->save('full.array', $data, 600); 
             return $data;
         }
-    }
-    
-    private function subTreeShow($nodes, $repo)
-    {
-        $ret  = '<ul>';
-        
-        foreach($nodes as $node)
-        {
-            $ret .= '<li>';
-            $ret .= $node;
-            if($repo->childCount($node, true))
-                $ret .= $this->subTreeShow($repo->children($node, true), $repo);
-            $ret .= '</li>';
-        }
-        
-        $ret .= '</ul>';
-        
-        return $ret;
     }
     
     private function subTreeContents($nodes, $repo)
